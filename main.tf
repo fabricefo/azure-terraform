11b18530f2a7dbcbf924c8c756ca8887e3e513e0
Fabrice Fourel <ffourel@gmail.com>
2025-04-24 14:01:59 +0200

    Modify tf files

--- main.tf
+++ main.tf
@@ -31,52 +31,23 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "test-vnet"
+  name                = lower(module.naming.virtual_network.name)
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = var.address_space
   tags                = var.tags
 }
 
-resource "azurerm_subnet" "public_subnet" {
-  name                 = "public-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+resource "azurerm_subnet" "vm_subnet" {
   address_prefixes     = var.address_snet_public
-
-  delegation {
-    name = "delegation"
-    service_delegation {
-      name = "Microsoft.ContainerInstance/containerGroups"
-    }
-  }
-}
-
-resource "azurerm_subnet" "private_subnet" {
-  name                 = "private-subnet"
+  name                 = lower("${module.naming.subnet.name}-Front")
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
+}
+
+resource "azurerm_subnet" "vm_subnet" {
   address_prefixes     = var.address_snet_private
-}
-
-resource "azurerm_network_security_group" "public_nsg" {
-  name                = "public-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_network_security_group" "private_nsg" {
-  name                = "private-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet_network_security_group_association" "public_nsg_assoc" {
-  subnet_id                 = azurerm_subnet.public_subnet.id
-  network_security_group_id = azurerm_network_security_group.public_nsg.id
-}
-
-resource "azurerm_subnet_network_security_group_association" "private_nsg_assoc" {
-  subnet_id                 = azurerm_subnet.private_subnet.id
-  network_security_group_id = azurerm_network_security_group.private_nsg.id
+  name                 = lower("${module.naming.subnet.name}-Back")
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
 }
--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,7 @@
-# backend.tf
-
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-ResourceGroup"
     storage_account_name = "terraformoptstoaccount"
-    container_name       = "tfstatefile"
-    key                  = "test.terraform.tfstate"
-    use_oidc             = true
+    container_name       = "tfstate"
   }
 }
