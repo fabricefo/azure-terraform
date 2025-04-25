97659328f28fa3b3d2a2206db336e2a0e9eb8cc9
Fabrice Fourel <ffourel@gmail.com>
2025-04-25 10:08:41 +0200

    Add nginx install on vm

--- avm_vm.tf
+++ avm_vm.tf
@@ -48,5 +48,17 @@ module "vm" {
     version   = "latest"
   }
 
+  # Script cloud-init pour installer Nginx
+  custom_data = <<-EOF
+    #cloud-config
+    package_update: true
+    package_upgrade: true
+    packages:
+      - nginx
+    runcmd:
+      - systemctl enable nginx
+      - systemctl start nginx
+  EOF
+
   tags = var.tags
 }
